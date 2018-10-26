8f7040013270797071a094c4a73bad2d0187790b
Anton <anton@adc.sh>
2018-10-26 13:11:01 +0300

    test reduce test suite

--- test/spec/reducer.js
+++ test/spec/reducer.js
@@ -8,9 +8,10 @@ const CONTEXT = [Private, { ...C }]
 const T = {
   context: CONTEXT,
   async 'reduces a single test'({ reducer }, { assertDates, deleteDates, test }) {
-    const res = await reducer([test])
-    assertDates(res)
-    const sd = deleteDates(res)
+    const res = await reducer({ test })
+    const vals = Object.values(res)
+    assertDates(vals)
+    const sd = deleteDates(vals)
     deepEqual(sd, [{
       result: undefined,
       error: null,
@@ -18,9 +19,10 @@ const T = {
     }])
   },
   async 'reduces 2 tests'({ reducer }, { assertDates, deleteDates, test }) {
-    const res = await reducer([test, test])
-    assertDates(res)
-    const sd = deleteDates(res)
+    const res = await reducer({ test, test2: test })
+    const vals = Object.values(res)
+    assertDates(vals)
+    const sd = deleteDates(vals)
     deepEqual(sd, [{
       result: undefined,
       error: null,
@@ -34,10 +36,10 @@ const T = {
   async 'reduces focussed test'({ reducer }, { deleteDates, makeFocused, test }) {
     const F = 'focused'
     const t = makeFocused(test, () => F)
-    const res = await reducer([t, test, test], {
+    const res = await reducer({ t, test, test2: test }, {
       onlyFocused: true,
     })
-    const sd = deleteDates(res)
+    const sd = deleteDates(Object.values(res))
     deepEqual(sd, [{
       result: F,
       error: null,
@@ -45,5 +47,32 @@ const T = {
     }])
   },
 }
+
+// export { T }
+
+/** @type {Object.<string, (api: Private, r0: C)>} */
+const TestSuite = {
+  context: CONTEXT,
+  async 'reduces test suite'({ reducer }, { test, deleteDates }) {
+    const ts = { tests: { test }, isTest: false }
+    const tests = { ts }
+    const res = await reducer(tests, {
+      onlyFocused: false,
+    })
+    const [t] = deleteDates([res.ts.test])
+    res.ts.test = t
+    deepEqual(res, {
+      ts: {
+        test: {
+          result: undefined,
+          error: null,
+          destroyResult: [],
+        },
+      },
+    })
+  },
+}
+
+export { TestSuite }
 
 export default T
