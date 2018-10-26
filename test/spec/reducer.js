dc9de38408e6d7d23e807f566fb0e2b55817978b
Anton <anton@adc.sh>
2018-10-26 13:30:23 +0300

    update reducer .

--- test/spec/reducer.js
+++ test/spec/reducer.js
@@ -8,7 +8,7 @@ const CONTEXT = [Private, { ...C }]
 const T = {
   context: CONTEXT,
   async 'reduces a single test'({ reducer }, { assertDates, deleteDates, test }) {
-    const res = await reducer({ test })
+    const res = await reducer([ test ])
     const vals = Object.values(res)
     assertDates(vals)
     const sd = deleteDates(vals)
@@ -19,7 +19,7 @@ const T = {
     }])
   },
   async 'reduces 2 tests'({ reducer }, { assertDates, deleteDates, test }) {
-    const res = await reducer({ test, test2: test })
+    const res = await reducer([ test, { ...test, name: 'test2' } ])
     const vals = Object.values(res)
     assertDates(vals)
     const sd = deleteDates(vals)
@@ -36,7 +36,7 @@ const T = {
   async 'reduces focussed test'({ reducer }, { deleteDates, makeFocused, test }) {
     const F = 'focused'
     const t = makeFocused(test, () => F)
-    const res = await reducer({ t, test, test2: test }, {
+    const res = await reducer([ t, test, { ...test, name: 'test2' } ], {
       onlyFocused: true,
     })
     const sd = deleteDates(Object.values(res))
@@ -48,21 +48,20 @@ const T = {
   },
 }
 
-// export { T }
-
 /** @type {Object.<string, (api: Private, r0: C)>} */
 const TestSuite = {
   context: CONTEXT,
-  async 'reduces test suite'({ reducer }, { test, deleteDates }) {
-    const ts = { tests: { test }, isTest: false }
-    const tests = { ts }
-    const res = await reducer(tests, {
+  async 'reduces test suite'({ reducer }, { test }) {
+    const ts = [{ tests: [ test ], isTest: false, name: 'test-suite' }]
+    const res = await reducer(ts, {
       onlyFocused: false,
     })
-    const [t] = deleteDates([res.ts.test])
-    res.ts.test = t
+    Object.values(res['test-suite']).forEach(v => {
+      delete v.started
+      delete v.finished
+    })
     deepEqual(res, {
-      ts: {
+      'test-suite': {
         test: {
           result: undefined,
           error: null,
