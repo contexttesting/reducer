3128161ecbdb0db6ece4ba9c94ffd2200c1d6579
Anton <anton@adc.sh>
2018-10-26 07:31:03 +0300

    b types

--- build/index.js
+++ build/index.js
@@ -1,43 +1,5 @@
 const $_lib_reducer = require('./lib/reducer');
 const $_lib_run_test = require('./lib/run-test');
 
-/* documentary types/context.xml */
-/**
- * @typedef {Object} Context A context made with a constructor.
- * @prop {() => void} [_init] A function to initialise the context.
- * @prop {() => void} [_destroy] A function to destroy the context.
- *
- * @typedef {{new(...args: any[]): Context}} ContextConstructor A function or class or object that makes a context
- */
-
-/* documentary types/reducer.xml */
-/**
- * @typedef {Object} Config Options for the reducer.
- * @prop {boolean} [onlyFocused=false] Run only focused tests. Default `false`.
- * @prop {function} notify The notify function to be passed to run method.
- */
-
-/* documentary types/run.xml */
-/**
- * @typedef {Object} Run Options for the run function.
- * @prop {ContextConstructor[]} [context] Any context constructors for the test to be evaluated.
- * @prop {number} [timeout=null] The timeout for the test, context evaluation and destruction. Default `null`.
- * @prop {function} fn The test function to run.
- */
-
-/* documentary types/test.xml */
-/**
- * @typedef {Object} Test The test type as used by the reducer.
- * @prop {ContextConstructor[]} [context] Any context constructors for the test to be evaluated.
- * @prop {number} [timeout=null] The timeout for the test, context evaluation and destruction. Default `null`.
- * @prop {number} name The name of the test.
- * @prop {boolean} [isFocused=false] If the test is focused. Default `false`.
- * @prop {boolean} [isTest=true] If it is a test and not a test suite. Default `true`.
- * @prop {boolean} [isSelfFocused] The property of the test suite such that it is focused.
- * @prop {boolean} [hasFocused] Whether the test suite has focused tests.
- * @prop {function} fn The test function to run.
- */
-
-
 module.exports.reducer = $_lib_reducer
 module.exports.runTest = $_lib_run_test
--- build/lib/index.js
+++ build/lib/index.js
@@ -67,13 +67,9 @@ process.env.ALAMODE_ENV == 'test-build' && console.log('> test %s', require('pat
 }
 
 
-/* documentary types/context.xml */
 /**
- * @typedef {Object} Context A context made with a constructor.
- * @prop {() => void} [_init] A function to initialise the context.
- * @prop {() => void} [_destroy] A function to destroy the context.
- *
- * @typedef {{new(...args: any[]): Context}} ContextConstructor A function or class or object that makes a context
+ * @typedef {import('@zoroaster/types').Context} Context
+ * @typedef {import('@zoroaster/types').ContextConstructor} ContextConstructor
  */
 
 
