3092e72d29422fcdf5206c7ad89cf7c40d49037b
Anton <anton@adc.sh>
2018-10-26 06:28:55 +0300

    import types

--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -59,24 +59,8 @@ const reducer = async (tests, config = {}) => {
 
 export default reducer
 
-/* documentary types/context.xml */
 /**
- * @typedef {Object} Context A context made with a constructor.
- * @prop {() => void} [_init] A function to initialise the context.
- * @prop {() => void} [_destroy] A function to destroy the context.
- *
- * @typedef {{new(...args: any[]): Context}} ContextConstructor A function or class or object that makes a context
- */
-
-/* documentary types/test.xml */
-/**
- * @typedef {Object} Test The test type as used by the reducer.
- * @prop {ContextConstructor[]} [context] Any context constructors for the test to be evaluated.
- * @prop {number} [timeout=null] The timeout for the test, context evaluation and destruction. Default `null`.
- * @prop {number} name The name of the test.
- * @prop {boolean} [isFocused=false] If the test is focused. Default `false`.
- * @prop {boolean} [isTest=true] If it is a test and not a test suite. Default `true`.
- * @prop {boolean} [isSelfFocused] The property of the test suite such that it is focused.
- * @prop {boolean} [hasFocused] Whether the test suite has focused tests.
- * @prop {function} fn The test function to run.
+ * @typedef {import('..').Context} Context
+ * @typedef {import('..').ContextConstructor} ContextConstructor
+ * @typedef {import('..').Test} Test
  */
